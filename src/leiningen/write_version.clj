a3e701b95227bc663496e9fa1ade3039e2c5d4b4
Casey Marshall <casey.s.marshall@gmail.com>
2016-10-04 12:34:48 -0700

    Add write-version task.
    Because Jenkins hates us all.

---
+++ src/leiningen/write_version.clj
@@ -0,0 +1,8 @@
+(ns leiningen.write-version)
+
+(defn write-version
+  "Write the current project version to 'outfile'.
+   Default outfile is 'version.properties'."
+  ([project] (write-version project "version.properties"))
+  ([project outfile]
+    (spit outfile (str "PROJECT_VERSION=" (:version project) "\n"))))
