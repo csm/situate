40772312d549c4aa70a33ee43f1c7defce55cbc9
Casey Marshall <casey.s.marshall@gmail.com>
2016-10-03 22:26:43 -0700

    I want to deploy to clojars, please.

--- project.clj
+++ project.clj
@@ -3,4 +3,13 @@
   :url "http://github.com/csm/lein-situate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
